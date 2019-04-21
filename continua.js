d2d390c1a6e55c43221f49c667bd38a61d32bdaa
rektide de la faye <rektide@voodoowarez.com>
2019-04-21 02:26:46 -0400

    Start rewriting as an iterator.

--- continua.js
+++ continua.js
@@ -1,17 +1,63 @@
 "use module"
 import findSignal from "./find-signal.js"
 
-let defaults
+
+const
+  $factory= Symbol.for( "continua:factory"),
+  $tick= Symbol.for( "continua:tick"),
+  $state= Symbol.for( "continua:state")
+
+export function continua( factory, options){
+	const self= {
+		factory,
+		tick,
+		state: null,
+		next: function(){
+			self.value
+			return self
+		},
+		value: ,
+		done: false
+	}
+	return self
+}
+
 
 export async function *continua( factory, options){
+	if( options){
+		Object.assign( this, options)
+	}
+
+	let defaults
+	if( !this.tick|| !this.diff){
+		defaults= (await import( "./defaults.js")).defaults
+	}
+	Object.defineProperties( self, {
+		tick: {
+			enumerable: true,
+			get: function(){
+				return tick
+			}
+			set: function( newTick){
+				const oldTick= tick
+				tick= newTick
+				return oldTick
+			}
+		}
+	})
+
+	if( !this.tick){
+		this	let tick
+
+	}
+
 	// find defaults
 	let
 	  optsTick= options&& options.tick,
+	  optsDiff= options&& options.diff,
 	  optsComparator= options&& options.comparator
-	if( !optsTick|| !optsComparator){
+	if( !optsTick|| !optsComparator|| !optsDiff){
 		if( !defaults){
-			const module = await import( "./defaults.js")
-			defaults= module.defaults
 		}
 		const d= await defaults()
 		optsTick= optsTick|| d.tick
@@ -23,29 +69,24 @@ export async function *continua( factory, options){
 	// yield it all
 	yield *state
 
+
+
 	// start time ticking
 	const
 	  tickOptions= options&& options.tickOptions!== undefined? options.tickOptions: options,
 	  ticker= optsTick( tickOptions)
 	// for every tick
-	for await( const tick of ticker){
-		// re-fetch the state
-		const newState= Array.from(await factory())
-		// in our new state,
-		NEW: for( const newer of newState){
-			// look for each element in our old state
-			for( const existing of state){
-				if( optsComparator( existing, newer)){
-					// continue on if we find the element in the old state
-					continue NEW
-				}
-			}
-			// and yield the element if it's not in old state
-			yield newer
+	try{
+		for await( const tick of ticker){
+			const newStates= await factory()
+			yield *this.diff( newStates)
 		}
-		// then capture our new state
-		state= newState
+	}finally{
+		if( options.finally){
+			options.finally.call( this)
+		}
 	}
+	
 }
 export const Continua = continua
 export default continua
