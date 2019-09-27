e2cf4a0ba8955a99710ff05d7fed6cd2995348b7
jauntywunderkind <jaunty+wunder+kind+git@voodoowarez.com>
2019-09-27 15:16:52 -0400

    pass more state around faster for factory.

--- factory.js
+++ factory.js
@@ -4,28 +4,37 @@ import AsyncIterPipe from "async-iter-pipe/async-iter-pipe.js"
 export class FactoryContinua extends AsyncIterPipe{
 	listeners= []
 	rerun= false
-	constructor( fn, tick, opts= {}){
+	constructor( producer, tick, opts= {}){
 		super( opts)
-		this.fn= fn
+		this.producer= producer
 		if( tick){
 			this.listenToTick( tick)
 		}
 	}
 
-	async listenToTick( ticker){
+	async listenToTick( res, rej, self, ticker){
+
 		// make listener state
 		const state= {
+			factory: this,
 			ticker,
-			iterator: ticker[ Symbol.asyncIteration](),
+			iterator: ticker[ Symbol.asyncIteration]? ticker[ Symbol.asyncIteration](): ticker[ Symbol.iteration]()
 			run: null,
 			findIndex: ()=> this.listeners.indexOf( state)
 		}
 		// remember state
-		this.listeners.push( state)
+		this.listeners.push( self)
 
+		// start run
+		state.processor= _run( state)
+		// return state
+		return state
+	}
+
+	async _run( state){
 		let cursor
 		while( true){
-			cursor= await state.iterator.next()
+			cursor= await self.iterator.next()
 			if( cursor.done){
 				break
 			}
@@ -35,7 +44,7 @@ export class FactoryContinua extends AsyncIterPipe{
 					if( state.rerun=== true|| state.rerun=== false){
 						state.rerun= false
 					}
-					state.run= this._run( cursor.value)
+					state.run= this._run( state)
 				}while( state.rerun)
 			}else if(state.rerun=== false){
 				// re-run once run
@@ -44,10 +53,9 @@ export class FactoryContinua extends AsyncIterPipe{
 		}
 		return cursor.value
 	}
-
-	_run( value){
-		for await( const item of this.fn( value)){
-			this.produce( item)
+	async _run( state){
+		for await( const item of this.producer( value)){
+			state.factory.produce( item)
 		}
 	}
 }
