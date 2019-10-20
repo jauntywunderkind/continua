a57cf497cd9b7ffb0201df4c5cb94ff3d7d12ccc
jauntywunderkind <jaunty+wunder+kind+git@voodoowarez.com>
2019-10-20 15:53:53 -0400

    continue wip base test for factory

--- .test/factory.js
+++ .test/factory.js
@@ -1,21 +1,27 @@
 "use module"
+import AsyncIterInterval from "async-iter-interval"
+import AsyncIterExpect from "async-iter-expect"
 import delay from "delay"
 import tape from "tape"
 
 import Factory from "continua/factory" // i haven't used package local scope before, only relative scope ..
 import { producer, tick} from "./stub.js" // but also relative
 
-import AsyncIterInterval from "async-iter-interval"
 
 tape( "build a factory which produces things", async function( t){
-	t.plan( 3)
+	t.plan( 1)
 	const
 	  producer= Producer(),
-	  tick= Tick(),
+	  delay= 3,
+	  tick= Tick( delay),
 	  opts= {},
-	  f= new Factory( producer, tick, {})
-	await delay( 6) // 
-	
+	  factory= new Factory( producer, tick, {}),
+	  expect= Expect( f, [[ 1], [ 2, 2], [ 3, 3]])
+
+	expect.then( function(){
+		t.ok()
+	})
+	await delay( (rate* 3)+ 2)
 })
 
 
