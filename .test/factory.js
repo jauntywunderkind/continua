a49bcce425ff10647bff904e133bc5144dc09d36
jauntywunderkind <jaunty+wunder+kind+git@voodoowarez.com>
2019-10-20 15:46:25 -0400

    factory instantiates one

--- .test/factory.js
+++ .test/factory.js
@@ -1,15 +1,20 @@
 "use module"
+import delay from "delay"
 import tape from "tape"
+
 import Factory from "continua/factory" // i haven't used package local scope before, only relative scope ..
+import { producer, tick} from "./stub.js" // but also relative
 
-tape( "build a factory which produces things", function( t){
-	function generator( n= generator.n){
-		const arr= new Array( generator.n)
-		arr.fill( generator.n)
-		return arr
-	}
-	generator.n= 1
+import AsyncIterInterval from "async-iter-interval"
 
+tape( "build a factory which produces things", async function( t){
+	t.plan( 3)
+	const
+	  producer= Producer(),
+	  tick= Tick(),
+	  opts= {},
+	  f= new Factory( producer, tick, {})
+	await delay( 6) // 
 	
 })
 
