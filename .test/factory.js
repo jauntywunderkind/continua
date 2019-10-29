663920d2b61065f1ad57bb5767c8d9b0feebadf1
jauntywunderkind <jaunty+wunder+kind+git@voodoowarez.com>
2019-10-29 14:30:20 -0400

    terminate factory test

--- .test/factory.js
+++ .test/factory.js
@@ -6,22 +6,23 @@ import tape from "tape"
 import Factory from "continua/factory" // i haven't used package local scope before, only relative scope ..
 import { Producer, Tick, DELAY} from "./stub.js" // but also relative
 
-
 tape( "build a factory which produces things", async function( t){
 	t.plan( 1)
 	const
 	  producer= Producer(),
-	  tick= Tick(),
+	  tick= Tick( DELAY),
 	  opts= {},
 	  factory= new Factory( producer, tick, opts),
-	  expect= new AsyncIterExpect( factory, [[ 1], [ 2, 2], [ 3, 3]])
+	  //expect= new AsyncIterExpect( factory, [[ 1], [ 2, 2], [ 3, 3, 3]])
+	  expect= new AsyncIterExpect( factory, [ 1, 2, 2, 3, 3, 3])
 
 	expect.then( function(){
 		t.ok()
 	}, function(){
 		t.fail()
 	})
-	await Delay( (DELAY* 3)+ 10)
+	await Delay(( DELAY* 3) + 5 )
+	tick.clearInterval()
 })
 
 
