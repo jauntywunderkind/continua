e9dc88d54f7963d639870ee390fc0354634fbd78
jauntywunderkind <jaunty+wunder+kind+git@voodoowarez.com>
2019-11-06 16:27:40 -0500

    Remove old factory code.

--- .test/stub.js
+++ .test/stub.js
@@ -1,5 +1,6 @@
 "use module"
 import AsyncIterInterval from "async-iter-interval"
+import delay from "delay"
 
 export function producer( n= producer.n, tap){
 	function producer(){
@@ -16,9 +17,20 @@ export function producer( n= producer.n, tap){
 }
 producer.n= 1
 
-export const DELAY= 5
+export const DELAY= 10
 
-export const tick= (n= DELAY)=> new AsyncIterInterval( n)
+export const tick1= (n= DELAY)=> new AsyncIterInterval( n)
+export function tick2(n){
+	return async function *(){
+		yield true;
+		await delay(10)
+		yield true;
+		await delay(10)
+		yield true;
+		await delay(10)
+	}
+}
+export const tick= tick1
 
 const index= {
   producer,
